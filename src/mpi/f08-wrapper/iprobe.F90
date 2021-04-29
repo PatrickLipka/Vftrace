a08cff7936af59bc5af94980eb351f2a40fa099c
Felix Uhl <Felix.Uhl@emea.nec.com>
2021-04-29 14:51:24 +0200

    added iprobe functionality and iprobe test for f08-mpi

---
+++ src/mpi/f08-wrapper/iprobe.F90
@@ -0,0 +1,39 @@
+! This file is part of Vftrace.
+!
+! Vftrace is free software; you can redistribute it and/or modify
+! it under the terms of the GNU General Public License as published by
+! the Free Software Foundation; either version 2 of the License, or
+! (at your option) any later version.
+!
+! Vftrace is distributed in the hope that it will be useful,
+! but WITHOUT ANY WARRANTY; without even the implied warranty of
+! MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+! GNU General Public License for more details.
+!
+! You should have received a copy of the GNU General Public License along
+! with this program; if not, write to the Free Software Foundation, Inc.,
+! 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+
+#ifdef _MPI
+
+SUBROUTINE MPI_Iprobe_f08(source, tag, comm, flag, status, error)
+   USE vftr_mpi_iprobe_f082c_f08interface, &
+      ONLY : vftr_MPI_Iprobe_f082c
+   USE mpi_f08, ONLY : MPI_Comm, &
+                       MPI_Status
+   IMPLICIT NONE
+   INTEGER, INTENT(IN) :: source
+   INTEGER, INTENT(IN) :: tag
+   TYPE(MPI_Comm), INTENT(IN) :: comm
+   LOGICAL, INTENT(OUT) :: flag
+   TYPE(MPI_Status) :: status
+   INTEGER, OPTIONAL, INTENT(OUT) :: error
+   INTEGER :: tmperror
+
+   CALL vftr_MPI_Iprobe_f082c(source, tag, comm%MPI_VAL, FLAG, status, tmperror)
+
+   IF (PRESENT(error)) error = tmperror
+
+END SUBROUTINE MPI_Iprobe_f08
+
+#endif 
---
+++ src/mpi/f082c/vftr_mpi_iprobe_f082c_f08interface.F90
@@ -0,0 +1,49 @@
+! This file is part of Vftrace.
+!
+! Vftrace is free software; you can redistribute it and/or modify
+! it under the terms of the GNU General Public License as published by
+! the Free Software Foundation; either version 2 of the License, or
+! (at your option) any later version.
+!
+! Vftrace is distributed in the hope that it will be useful,
+! but WITHOUT ANY WARRANTY; without even the implied warranty of
+! MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+! GNU General Public License for more details.
+!
+! You should have received a copy of the GNU General Public License along
+! with this program; if not, write to the Free Software Foundation, Inc.,
+! 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+
+MODULE vftr_mpi_iprobe_f082c_f08interface
+#ifdef _MPI
+
+   USE, INTRINSIC :: ISO_FORTRAN_ENV
+   USE, INTRINSIC :: ISO_C_BINDING
+
+   IMPLICIT NONE
+
+   PRIVATE
+
+   PUBLIC :: vftr_MPI_Iprobe_f082c
+
+   INTERFACE
+
+      SUBROUTINE vftr_MPI_Iprobe_f082c(source, tag, f_comm, f_flag, f_status, f_error) &
+         BIND(C, name="vftr_MPI_Iprobe_f082c")
+         USE mpi_f08, ONLY : MPI_Status
+         IMPLICIT NONE
+         INTEGER, INTENT(IN) :: source
+         INTEGER, INTENT(IN) :: tag
+         INTEGER, INTENT(IN) :: f_comm
+         LOGICAL, INTENT(OUT) :: f_flag
+         TYPE(MPI_Status) :: f_status
+         INTEGER, INTENT(OUT) :: f_error
+      END SUBROUTINE vftr_MPI_Iprobe_f082c
+
+   END INTERFACE
+
+#endif 
+
+CONTAINS
+
+END MODULE vftr_mpi_iprobe_f082c_f08interface
